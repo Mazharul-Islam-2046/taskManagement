6eab065c290a555b6a76f3717701c79471350457
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2023-12-22 15:33:06 +0600

    DND Application

--- src/Pages/TaskManagement/SubPages/All/All.jsx
+++ src/Pages/TaskManagement/SubPages/All/All.jsx
@@ -1,33 +1,116 @@
 import { useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-
-const tasks = [
-    {
-        task: 1
-    },
-    {
-        task: 2
-    },
-    {
-        task: 3
-    },
-    {
-        task: 4
-    }
-]
-
-
-
+const toDoData = [
+  {
+    task: 1,
+  },
+  {
+    task: 2,
+  },
+  {
+    task: 3,
+  },
+  {
+    task: 4,
+  },
+];
+const inProgressData = [
+  {
+    task: 1,
+  },
+  {
+    task: 2,
+  },
+];
+const completeData = [
+  {
+    task: 1,
+  },
+  {
+    task: 2,
+  },
+  {
+    task: 3,
+  },
+  {
+    task: 4,
+  },
+  {
+    task: 5
+  }
+];
 
 const All = () => {
+  const [todoTasks, setTodoTasks] = useState(toDoData);
+  const [inProgressTasks, setInProgressTasks] = useState(inProgressData);
+  const [completeTasks, setCompleteTasks] = useState(completeData);
+
+  const handleDrag = (results) => {
+
+    const { source, destination, type } = results;
+
+
+    // Never Droped
+    if (!destination) return;
+
+
+    // Dropped in the same Place
+    if (
+      source.droppableId === destination.droppableId &&
+      source.index === destination.index
+    ) return;
 
 
 
-    const [todoTasks, setTodoTasks] = useState(tasks)
+    // Dropped in the same box but Different place TODO
+    if (type === "todo" && source.droppableId === destination.droppableId) {
+      const reorderedToDoTasks = [...todoTasks];
 
-  const handleDrag = ( source, destination, type ) => {
-    console.log("Draged");
+      const toDoTaskSourceIndex = source.index;
+      const toDoTaskDestinatonIndex = destination.index;
+
+      const [kickOutToDoTask] = reorderedToDoTasks.splice(toDoTaskSourceIndex, 1);
+      reorderedToDoTasks.splice(toDoTaskDestinatonIndex, 0, kickOutToDoTask);
+
+      return setTodoTasks(reorderedToDoTasks)
+    }
+
+
+
+
+    // Dropped in the same box but Different place INPROGRESS
+    if (type === "inprogress" && source.droppableId === destination.droppableId) {
+      const reorderedToDoTasks = [...inProgressTasks];
+
+      const toDoTaskSourceIndex = source.index;
+      const toDoTaskDestinatonIndex = destination.index;
+
+      const [kickOutToDoTask] = reorderedToDoTasks.splice(toDoTaskSourceIndex, 1);
+      reorderedToDoTasks.splice(toDoTaskDestinatonIndex, 0, kickOutToDoTask);
+
+      return setInProgressTasks(reorderedToDoTasks)
+    }
+
+
+
+
+    // Dropped in the same box but Different place COMPLETE
+    if (type === "complete" && source.droppableId === destination.droppableId) {
+      const reorderedToDoTasks = [...completeTasks];
+
+      const toDoTaskSourceIndex = source.index;
+      const toDoTaskDestinatonIndex = destination.index;
+
+      const [kickOutToDoTask] = reorderedToDoTasks.splice(toDoTaskSourceIndex, 1);
+      reorderedToDoTasks.splice(toDoTaskDestinatonIndex, 0, kickOutToDoTask);
+
+      return setCompleteTasks(reorderedToDoTasks)
+    }
+
+
+
+    // Dropped in the different box
   };
 
   return (
@@ -35,7 +118,7 @@ const All = () => {
       <DragDropContext onDragEnd={handleDrag}>
         <div className="py-12 grid grid-cols-3 gap-16">
           {/* To Do Box */}
-          <Droppable droppableId="todo" type="group">
+          <Droppable droppableId="todo" type="inprogress">
             {(provided) => (
               <div
                 {...provided.droppableProps}
@@ -47,23 +130,27 @@ const All = () => {
                   To Do
                 </h3>
                 <div className="mt-6 space-y-4">
-                  {todoTasks.map( (task, index) => (
-                  <Draggable draggableId={`task-${index}`} index={index} key={index}>
-                    {(provided) => (
-                      <div
-                        {...provided.dragHandleProps}
-                        {...provided.draggableProps}
-                        ref={provided.innerRef}
-                        className="py-3 px-4 bg-yellow-400 text-white rounded-md"
-                      >
-                        <h4 className="text-lg font-bold text-left">Task {task.task}</h4>
-                      </div>
-                    )}
-                  </Draggable>
-                  )
-                  )
-                  }
-                  
+                  {todoTasks.map((task, index) => (
+                    <Draggable
+                      draggableId={`toDoTask-${index}`}
+                      index={index}
+                      key={index}
+                    >
+                      {(provided) => (
+                        <div
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                          ref={provided.innerRef}
+                          className="py-3 px-4 bg-yellow-400 text-white rounded-md"
+                        >
+                          <h4 className="text-lg font-bold text-left">
+                            Task {task?.task}
+                          </h4>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+
                   {provided.placeholder}
                 </div>
               </div>
@@ -71,34 +158,84 @@ const All = () => {
           </Droppable>
 
           {/* In Progress Box */}
-          <div className="w-full border-4 border-[#3498db] py-5 px-8">
-            {/* Title */}
-            <h3 className="text-2xl text-left uppercase font-bold">
-              In Progress
-            </h3>
-            <div className="mt-6 space-y-4">
-              <div className="py-3 px-4 bg-yellow-400 text-white rounded-md">
-                <h4 className="text-lg font-bold text-left">Task 1</h4>
+          <Droppable droppableId="inprogress" type="inprogress">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="w-full border-4 border-[#3498db] py-5 px-8"
+              >
+                {/* Title */}
+                <h3 className="text-2xl text-left uppercase font-bold">
+                  To Do
+                </h3>
+                <div className="mt-6 space-y-4">
+                  {inProgressTasks.map((task, index) => (
+                    <Draggable
+                      draggableId={`inProgressTask-${index}`}
+                      index={index}
+                      key={index}
+                    >
+                      {(provided) => (
+                        <div
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                          ref={provided.innerRef}
+                          className="py-3 px-4 bg-yellow-400 text-white rounded-md"
+                        >
+                          <h4 className="text-lg font-bold text-left">
+                            Task {task?.task}
+                          </h4>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+
+                  {provided.placeholder}
+                </div>
               </div>
-              <div className="py-3 px-4 bg-yellow-400 text-white rounded-md">
-                <h4 className="text-lg font-bold text-left">Task 1</h4>
-              </div>
-            </div>
-          </div>
+            )}
+          </Droppable>
 
           {/* Complete Box */}
-          <div className="w-full border-4 border-[#3498db] py-5 px-8">
-            {/* Title */}
-            <h3 className="text-2xl text-left uppercase font-bold">Complete</h3>
-            <div className="mt-6 space-y-4">
-              <div className="py-3 px-4 bg-yellow-400 text-white rounded-md">
-                <h4 className="text-lg font-bold text-left">Task 1</h4>
+          <Droppable droppableId="complete" type="complete">
+            {(provided) => (
+              <div
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className="w-full border-4 border-[#3498db] py-5 px-8"
+              >
+                {/* Title */}
+                <h3 className="text-2xl text-left uppercase font-bold">
+                  To Do
+                </h3>
+                <div className="mt-6 space-y-4">
+                  {completeTasks.map((task, index) => (
+                    <Draggable
+                      draggableId={`completeTask-${index}`}
+                      index={index}
+                      key={index}
+                    >
+                      {(provided) => (
+                        <div
+                          {...provided.dragHandleProps}
+                          {...provided.draggableProps}
+                          ref={provided.innerRef}
+                          className="py-3 px-4 bg-yellow-400 text-white rounded-md"
+                        >
+                          <h4 className="text-lg font-bold text-left">
+                            Task {task?.task}
+                          </h4>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+
+                  {provided.placeholder}
+                </div>
               </div>
-              <div className="py-3 px-4 bg-yellow-400 text-white rounded-md">
-                <h4 className="text-lg font-bold text-left">Task 1</h4>
-              </div>
-            </div>
-          </div>
+            )}
+          </Droppable>
         </div>
       </DragDropContext>
     </div>
